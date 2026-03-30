6a863a338efdfde054d70e21941b04fc98b905d6
RiciT <tamas.balint.farago@gmail.com>
2026-03-30 11:27:33 +0200

    added canfocus patch

--- config.h
+++ config.h
@@ -73,9 +73,10 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Conky", NULL, NULL, ~0, 1, -1},
+    /* class      instance    title       tags mask     isfloating  canfocus
+       monitor */
+    {"Gimp", NULL, NULL, 0, 1, 1, -1},
+    {"Conky", NULL, NULL, ~0, 1, 0, -1},
 };
 
 /* layout(s) */
--- dwm.c
+++ dwm.c
@@ -165,8 +165,8 @@ struct Client {
   int basew, baseh, incw, inch, maxw, maxh, minw, minh, hintsvalid;
   int bw, oldbw;
   unsigned int tags;
-  int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen,
-      isforegrounded;
+  int isfixed, isfloating, canfocus, isurgent, neverfocus, oldstate,
+      isfullscreen, isforegrounded;
 
   Client *swallower;
   Client *swallowed;
@@ -243,6 +243,7 @@ typedef struct {
   const char *title;
   unsigned int tags;
   int isfloating;
+  int canfocus;
   int monitor;
 } Rule;
 
@@ -457,6 +458,7 @@ void applyrules(Client *c) {
 
   /* rule matching */
   c->isfloating = 0;
+  c->canfocus = 1;
   c->tags = 0;
   XGetClassHint(dpy, c->win, &ch);
   class = ch.res_class ? ch.res_class : broken;
@@ -468,6 +470,7 @@ void applyrules(Client *c) {
         (!r->class || strstr(class, r->class)) &&
         (!r->instance || strstr(instance, r->instance))) {
       c->isfloating = r->isfloating;
+      c->canfocus = r->canfocus;
       c->tags |= r->tags;
       for (m = mons; m && m->num != r->monitor; m = m->next)
         ;
@@ -1059,6 +1062,8 @@ void focus(Client *c) {
   if (selmon->sel && selmon->sel != c)
     unfocus(selmon->sel, 0);
   if (c) {
+    if (!c->canfocus)
+      return;
     if (c->mon != selmon)
       selmon = c->mon;
     if (c->isurgent)
@@ -1102,18 +1107,20 @@ void focusstack(const Arg *arg) {
   if (!selmon->sel || (selmon->sel->isfullscreen && lockfullscreen))
     return;
   if (arg->i > 0) {
-    for (c = selmon->sel->next; c && !ISVISIBLE(c); c = c->next)
+    for (c = selmon->sel->next; c && (!ISVISIBLE(c) || !c->canfocus);
+         c = c->next)
       ;
     if (!c)
-      for (c = selmon->clients; c && !ISVISIBLE(c); c = c->next)
+      for (c = selmon->clients; c && (!ISVISIBLE(c) || !c->canfocus);
+           c = c->next)
         ;
   } else {
     for (i = selmon->clients; i != selmon->sel; i = i->next)
-      if (ISVISIBLE(i))
+      if (ISVISIBLE(i) && i->canfocus)
         c = i;
     if (!c)
       for (; i; i = i->next)
-        if (ISVISIBLE(i))
+        if (ISVISIBLE(i) && i->canfocus)
           c = i;
   }
   if (c) {
